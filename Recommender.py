7cd76fd9f18c710ab228f4f1d604cf6458470580
Mahaly <m.razafiman@alustudent.com>
2020-04-20 19:53:31 +0300

    recommender class

---
+++ Recommender.py
@@ -0,0 +1,21 @@
+import Storage
+
+class Recommender:
+    def parseData(self):
+        pass
+
+    def generateListBasedHighestParam(self, type, genre, airedFrom):
+        pass
+
+    def generateListBasedPopularity(self, type, genre, airedFrom):
+        pass
+
+    def generateListBasedRating(self, type, genre, airedFrom):
+        pass
+
+    def generateListBasedCustom(self, param): # Still needs to be considered
+        pass
+
+    def generateListBasedRandom(self): 
+        pass
+
